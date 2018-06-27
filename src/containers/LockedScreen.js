dc7c70246f613ceed8435c4f7ac3e19828988ecb
whausman <whausman@servercentral.com>
2018-06-27 01:02:46 -0500

    keypad almost done

--- src/containers/LockedScreen.js
+++ src/containers/LockedScreen.js
@@ -1,117 +1,52 @@
 import React, { Component } from 'react';
 import { Motion, spring } from 'react-motion';
+import ActiveKey from './components/ActiveKey';
 //import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 //import Button from '@material-ui/core/Button';
+import xrpIcon from '../images/xrp-icon.svg';
 
 export default class LockScreen extends Component {
    constructor(props) {
       super(props);
 
       this.state = {
-         pinInput: '',
-         blink: true
+         pinInput: ''
       };
 
       this.validKey = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
       document.onkeydown = function(e) {
          e = e || window.event;
-         console.log(e);
-
-         if (this.validKey.indexOf(e.key) !== -1 && this.state.pinInput.length < 4) {
-            this.setState({
-               pinInput: this.state.pinInput + e.key
-            });
-         } else if (e.keyCode === 8) {
-            this.setState({
-               pinInput: this.state.pinInput.slice(0, -1)
-            });
-         }
-
-         console.log(this.state.pinInput);
+         this.updatePinInput(e.key, e.keyCode);
       }.bind(this);
+   }
 
-      //Toggle blinking pin input
-      // setInterval(
-      //    function() {
-      //       this.setState({
-      //          //blink: !this.state.blink
-      //          bounceHeight: 0
-      //       });
-      //    }.bind(this),
-      //    600
-      // );
+   updatePinInput(key, keyCode) {
+      if (this.validKey.indexOf(key) !== -1 && this.state.pinInput.length < 4) {
+         this.setState({
+            pinInput: this.state.pinInput + key
+         });
+      } else if (keyCode === 8 || keyCode === 'X') {
+         this.setState({
+            pinInput: this.state.pinInput.slice(0, -1)
+         });
+      }
+      console.log(this.state.pinInput);
    }
 
    render() {
-      const dialPad = [1, 2, 3, 4, 5, 6, 7, 8, 9, '?', 0, 'X'];
-      const pinInput = [0, 1, 2, 3];
-
-      //const springConfig = { stiffness: 180, damping: 6 };
-      const config = { stiffness: 140, damping: 14 };
-      //const toCSS = rotateX => ({ transform: `rotateX: ${rotateX}deg` });
+      const dialPad = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '?', '0', 'X'];
 
       const PinButtons = () => {
          return dialPad.map((num, i) => {
             return (
                <div key={`pin${i}`} className={'pinBtnWrap'}>
-                  <div className={'pinBtn'}>{num}</div>
+                  <div onClick={() => this.updatePinInput(num, num)} className={'pinBtn'}>
+                     {num}
+                  </div>
                </div>
             );
-         });
-      };
-
-      const PinInput = () => {
-         return pinInput.map((num, i) => {
-            if (i >= this.state.pinInput.length) {
-               // EMPTY
-               return (
-                  <Motion defaultStyle={{ rotateX: 0 }} style={{ rotateX: spring(this.state.bounceHeight, config) }}>
-                     {value => (
-                        <div
-                           key={`pinInput${i}`}
-                           style={{
-                              display: 'flex',
-                              width: '20%',
-                              height: 50
-                              //transform: `rotateY: ${value.rotateX}deg`
-                           }}>
-                           <div
-                              style={{
-                                 alignSelf: 'flex-end',
-                                 width: '100%',
-                                 height: 2,
-                                 //background: this.state.blink ? '#21c2f8' : 'none'
-                                 background: '#21c2f8',
-                                 marginBottom: value.rotateX
-                                 //transform: `rotateX: ${value.rotateX}`
-                                 //transform: `rotateX: ${value.rotateX}deg`
-                              }}
-                           />
-                        </div>
-                     )}
-                  </Motion>
-               );
-            } else {
-               // FILLED
-               return (
-                  <div
-                     key={`pinInput${i}`}
-                     style={{ display: 'flex', justifyContent: 'center', width: '20%', height: 50 }}>
-                     <div
-                        style={{
-                           width: '50%',
-                           height: '50%',
-                           background: '#ffffff',
-                           opacity: 0.9,
-                           borderRadius: 30,
-                           marginTop: 10
-                        }}
-                     />
-                  </div>
-               );
-            }
          });
       };
 
@@ -125,19 +60,32 @@ export default class LockScreen extends Component {
                background: '#202020'
             }}>
             <div style={{ maxWidth: 300 }}>
-               <h2 style={{ fontSize: 50, fontWeight: 100, color: '#ffffff', margin: '30px 0 15px 0' }}>xLedg</h2>
+               {/*TITLE*/}
+               <div
+                  style={{
+                     display: 'flex',
+                     justifyContent: 'center',
+                     margin: '30px 0 15px 0'
+                  }}>
+                  <img
+                     src={xrpIcon}
+                     style={{ maxWidth: 100, maxHeight: 100 }}
+                     alt={'xLedg - XRPL Decentralized Exchange'}
+                  />
+                  <div
+                     style={{ fontSize: 50, fontWeight: 100, color: '#ffffff', alignSelf: 'center', marginLeft: -10 }}>
+                     ledg
+                  </div>
+               </div>
 
                <div style={{ display: 'flex', margin: '15px 20px 30px 20px', justifyContent: 'space-between' }}>
-                  {<PinInput />}
+                  <ActiveKey length={this.state.pinInput.length - 1} i={0} />
+                  <ActiveKey length={this.state.pinInput.length - 1} i={1} />
+                  <ActiveKey length={this.state.pinInput.length - 1} i={2} />
+                  <ActiveKey length={this.state.pinInput.length - 1} i={3} />
                </div>
 
                <div>{<PinButtons />}</div>
-
-               {/*<Link to="/dashboard">*/}
-               {/*<Button variant="contained" color="primary">*/}
-               {/*Create PIN*/}
-               {/*</Button>*/}
-               {/*</Link>*/}
             </div>
          </div>
       );
---
+++ src/containers/components/ActiveKey.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Motion, spring } from 'react-motion';
+
+//import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+export default class ActiveKey extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {};
+   }
+
+   render() {
+      const springConfig = { stiffness: 80, damping: 10 };
+      const springRadiusConfig = { stiffness: 120, damping: 60 };
+
+      const ActiveKeyToggle = () => {
+         if (this.props.i === this.props.length) {
+            // ACTIVE KEY
+            return (
+               <Motion
+                  defaultStyle={{
+                     width: 60,
+                     height: 2,
+                     radius: 0,
+                     offset: 0,
+                     active: 0,
+                     red: 33,
+                     green: 194,
+                     blue: 248
+                  }}
+                  style={{
+                     width: spring(30, springConfig),
+                     height: spring(30, springConfig),
+                     radius: spring(30, springRadiusConfig),
+                     offset: spring(10, springConfig),
+                     red: spring(255, springConfig),
+                     green: spring(255, springConfig),
+                     blue: spring(255, springConfig),
+                     active: 1
+                  }}>
+                  {value => (
+                     <div
+                        style={{
+                           display: 'flex',
+                           width: '20%',
+                           height: 50,
+                           justifyContent: 'space-around'
+                        }}>
+                        <div
+                           style={{
+                              alignSelf: 'flex-end',
+                              width: value.width,
+                              height: value.height,
+                              backgroundColor: `rgba(${value.red}, ${value.green}, ${value.blue}, 1)`,
+                              borderRadius: value.radius,
+                              marginBottom: value.offset
+                           }}
+                        />
+                     </div>
+                  )}
+               </Motion>
+            );
+         } else if (this.props.i <= this.props.length) {
+            // FULL
+            return (
+               <div style={{ display: 'flex', justifyContent: 'center', width: '20%', height: 50 }}>
+                  <div
+                     style={{
+                        alignSelf: 'flex-end',
+                        width: 30,
+                        height: 30,
+                        background: '#ffffff',
+                        borderRadius: 30,
+                        marginBottom: 10
+                     }}
+                  />
+               </div>
+            );
+         } else {
+            // EMPTY
+            return (
+               <div style={{ display: 'flex', justifyContent: 'center', width: '20%', height: 50 }}>
+                  <div
+                     style={{
+                        alignSelf: 'flex-end',
+                        width: '100%',
+                        height: 2,
+                        background: '#21c2f8'
+                     }}
+                  />
+               </div>
+            );
+         }
+      };
+
+      return <ActiveKeyToggle />;
+   }
+}
+
+ActiveKey.propTypes = {
+   length: PropTypes.number,
+   i: PropTypes.number
+};
